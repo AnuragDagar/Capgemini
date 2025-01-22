5fef471241538661e397e34cb6fb9f95b0797594
Anuragdagar <anuragdagar8126@gmail.com>
2025-01-22 12:31:14 +0530

    assignment 22-01-2025

---
+++ Findage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Findage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int birthyear = sc.nextInt();
+        int Curryear = 2024;
+        int age = Curryear - birthyear;
+        System.out.println(age);
+    }
+}
